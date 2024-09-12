66c3781ad05313567c738fb39d07a3883d62d99f
Odizinne <flora.aubry@proton.me>
2024-09-12 11:43:21 +0200

    added fr translation

--- src/resources/tr/mousereactivergb_fr.ts
+++ src/resources/tr/mousereactivergb_fr.ts
@@ -1,161 +1,161 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
-  <context>
+<TS version="2.1" language="fr_FR">
+<context>
     <name>MouseReactiveRGB</name>
     <message>
-      <location filename="..\..\mousereactivergb.py" line="125" />
-      <source>Connected ✅</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.py" line="125"/>
+        <source>Connected ✅</source>
+        <translation>Connecté ✅</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <location filename="..\..\mousereactivergb.py" line="130" />
-      <source>Disconnected ❌</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <location filename="../../mousereactivergb.py" line="130"/>
+        <source>Disconnected ❌</source>
+        <translation>Déconnecté ❌</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.py" line="342" />
-      <location filename="..\..\mousereactivergb.py" line="286" />
-      <source>Stop effect</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.py" line="342"/>
+        <location filename="../../mousereactivergb.py" line="286"/>
+        <source>Stop effect</source>
+        <translation>Arrêter l&apos;effet</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.py" line="322" />
-      <source>Exit</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.py" line="322"/>
+        <source>Exit</source>
+        <translation>Quitter</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.py" line="324" />
-      <source>Show</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.py" line="324"/>
+        <source>Show</source>
+        <translation>Afficher</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <location filename="..\..\mousereactivergb.py" line="345" />
-      <source>Start effect</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <location filename="../../mousereactivergb.py" line="345"/>
+        <source>Start effect</source>
+        <translation>Démarrer l&apos;effet</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.py" line="391" />
-      <source>Custom</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.py" line="391"/>
+        <source>Custom</source>
+        <translation>Personnalisé</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.py" line="392" />
-      <source>Random</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.py" line="392"/>
+        <source>Random</source>
+        <translation>Aléatoire</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.py" line="394" />
-      <source>Accent</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.py" line="394"/>
+        <source>Accent</source>
+        <translation>Accent</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.py" line="396" />
-      <source>Any button</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.py" line="396"/>
+        <source>Any button</source>
+        <translation>N&apos;importe quel bouton</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.py" line="397" />
-      <source>Left button</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.py" line="397"/>
+        <source>Left button</source>
+        <translation>Bouton gauche</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.py" line="398" />
-      <source>Left / Right</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.py" line="398"/>
+        <source>Left / Right</source>
+        <translation>Gauche / Droite</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <location filename="..\..\mousereactivergb.py" line="402" />
-      <source>Mouse Reactive RGB</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <location filename="../../mousereactivergb.py" line="402"/>
+        <source>Mouse Reactive RGB</source>
+        <translation>Mouse Reactive RGB</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.py" line="403" />
-      <source>The application is still running in the background.</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.py" line="403"/>
+        <source>The application is still running in the background.</source>
+        <translation>L&apos;application s&apos;exécute toujours en arrière-plan.</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>General Settings</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>General Settings</source>
+        <translation>Paramètres généraux</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>Fade on release</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>Fade on release</source>
+        <translation>Fondu au relâchement</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>Saturation</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>Saturation</source>
+        <translation>Saturation</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>Brightness</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>Brightness</source>
+        <translation>Luminosité</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>FPS</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>FPS</source>
+        <translation>FPS</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>Fade duration</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>Fade duration</source>
+        <translation>Durée du fondu</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>Color mode</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>Color mode</source>
+        <translation>Mode de couleur</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>R</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>R</source>
+        <translation>R</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>G</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>G</source>
+        <translation>G</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>B</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>B</source>
+        <translation>B</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>Hex</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>Hex</source>
+        <translation>Hex</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>Colors</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>Colors</source>
+        <translation>Couleurs</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>Autostart effect</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>Autostart effect</source>
+        <translation>Démarrage automatique de l&apos;effet</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>OpenRGB SDK IP / Port</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>OpenRGB SDK IP / Port</source>
+        <translation>OpenRGB SDK IP / Port</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>Trigger effect on</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>Trigger effect on</source>
+        <translation>Déclencher l&apos;effet sur</translation>
     </message>
     <message>
-      <location filename="..\..\mousereactivergb.ui" line="0" />
-      <source>Reactive effect settings</source>
-      <translation type="unfinished" />
+        <location filename="../../mousereactivergb.ui" line="0"/>
+        <source>Reactive effect settings</source>
+        <translation>Paramètres de l&apos;effet réactif</translation>
     </message>
-  </context>
+</context>
 </TS>
